f063224048d85587946b216682535054e0a3f2f6
Emanuele Faraci <emanuelee.faraci@gmail.com>
2026-03-02 17:35:30 +0100

    Update MinecraftClientMixin.java

--- src/main/java/com/emqnuele/quickresourcepack/mixin/MinecraftClientMixin.java
+++ src/main/java/com/emqnuele/quickresourcepack/mixin/MinecraftClientMixin.java
@@ -15,27 +15,9 @@ public class MinecraftClientMixin {
     @Inject(method = "setOverlay", at = @At("HEAD"), cancellable = true)
     private void onSetOverlay(Overlay overlay, CallbackInfo ci) {
         if (ResourcePackHandler.isSilentReload) {
-            // If we are in silent reload mode, prevent the overlay from being set.
-            // This effectively hides the loading screen.
-            // We might want to allow some overlays, but definitely not the resource reload
-            // one.
-            // Usually resource reload uses SplashOverlay or similar.
-
             if (overlay instanceof SplashOverlay) {
                 ci.cancel();
             }
-            // If it's null (clearing overlay), we should probably allow it,
-            // or maybe we don't care because we never set it in the first place.
-            // But to be safe, if we are silent reloading, we just don't want *new* loading
-            // screens.
-            // If the game tries to clear the overlay (overlay == null), we let it pass
-            // just in case there was one before (though there shouldn't be).
-            // Actually, if we cancel setting it to null, we might get stuck with an old
-            // overlay?
-            // No, because we prevented the old one.
-
-            // Let's be specific: if it's a SplashOverlay (loading screen), cancel it.
-            // If it's null, let it happen (clearing).
         }
     }
 }
